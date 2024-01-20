9cf00d36253e2a36c8f2dda04f0b7bc67d63c26a
Hamza Zeadah <h.zeadah@media.gov.sa>
2024-01-20 13:47:49 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'blogs', component: BlogsComponent },
+  // Add more routes as needed
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,78 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { HomeCardComponent } from './home-card/home-card.component';
+import { UsersComponent } from './users/users.component';
+import { UserCardComponent } from './user-card/user-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { ResentBplgsComponent } from './resent-bplgs/resent-bplgs.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderuserComponent } from './headeruser/headeruser.component';
+import { UserTabelComponent } from './user-tabel/user-tabel.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+import {MatBadgeModule} from '@angular/material/badge';
+import { BlogsComponent } from './blogs/blogs.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeCardComponent,
+    UsersComponent,
+    UserCardComponent,
+    NavComponent,
+    ResentBplgsComponent,
+    HeaderComponent,
+    HeaderuserComponent,
+    UserTabelComponent,
+    AppToolbarComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    HttpClientModule ,
+    MatCardModule,
+    FlexLayoutModule,
+    LayoutModule,
+    MatGridListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatTooltipModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSortModule,
+    MatSelectModule,
+    MatCheckboxModule ,
+    MatToolbarModule ,
+    MatCardModule,
+    MatBadgeModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
